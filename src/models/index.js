2b1618c1f1fa11b43a662fc537b7924ae4e782d6
Aditya <2002adityaprakash@gmail.com>
2024-12-16 16:27:41 +0530

    1st commit

---
+++ src/models/index.js
@@ -0,0 +1,11 @@
+const Booking = require("./booking")
+const User = require("./user")
+const House = require("./house")
+
+const models = {
+    Booking,
+    User,
+    House
+}
+
+module.exports = models;
